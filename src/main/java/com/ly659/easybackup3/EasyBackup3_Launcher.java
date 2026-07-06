aa0538f9b3a083c467060d063588c285e5e7e2aa
Ly659 <2011202398@qq.com>
2026-07-06 18:01:39 +0800

    [2026.07.06] 新增全局变量tempDir，新增自动清理临时文件逻辑

--- src/main/java/com/ly659/easybackup3/EasyBackup3_Launcher.java
+++ src/main/java/com/ly659/easybackup3/EasyBackup3_Launcher.java
@@ -3,6 +3,11 @@ package com.ly659.easybackup3;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Objects;
+
 /**
  * 本类是EasyBackup3软件的启动程序，负责储存全局常量、启动UI界面。
  * <br>这些全局常量是静态的，可以通过类直接访问。
@@ -12,12 +17,43 @@ public class EasyBackup3_Launcher {
     public static Display display;
     public static Shell shell;
 
+    // 路径变量
+    public static File tempDir;     // 临时目录
+
+    /**
+     * 清理临时文件夹中的所有文件和目录。临时文件夹本身不会被删除。
+     * @param tempDir 临时文件夹路径
+     */
+    static void cleanupTempDir(File tempDir) {
+        // 循环遍历：该目录下的所有文件和文件夹
+        for (File tempFile : Objects.requireNonNull(tempDir.listFiles())) {
+            if (tempFile.isFile()) {
+                // 若是一个文件，则删除
+                if (!tempFile.delete()) {
+                    System.err.println("Could not delete temp file " + tempFile.getAbsolutePath());
+                }
+            } else if (tempFile.isDirectory()) {
+                // 若是一个文件夹，则递归删除其中的所有文件后，再删除其本身
+                cleanupTempDir(tempFile);
+                if (!tempFile.delete()) {
+                    System.err.println("Could not delete temp folder " + tempFile.getAbsolutePath());
+                }
+            }
+        }
+    }
+
     static void main() {
+        // 给变量赋值
+        tempDir = new File(System.getProperty("java.io.tmpdir"));       // 临时目录
+
         display = new Display();
         EasyBackup3 easyBackup3 = new EasyBackup3(display);
         shell = easyBackup3.shell;
 
         // 启动程序UI界面
         easyBackup3.open();
+
+        // 程序结束后，清理临时文件
+        cleanupTempDir(tempDir);
     }
 }
